7b8296b9d0367fb9369bafdf26f1d13d668339fc
Tahmid Shakil <at.shakil.92@gmail.com>
2017-04-27 03:36:45 +0600

    API skeleton

--- lib/rtime/rts.rb
+++ lib/rtime/rts.rb
@@ -1,4 +1,24 @@
 module RTime
   class RTS
+    class << self
+      def _time_for options
+      end
+
+      def _parse_date date
+      end
+
+      def _selections_for_coverage total, coverage
+      end
+
+      def _timestamps_in_range date1, date2, options
+      end
+
+      def timestamps(date1, date2, coverage, options={
+        night_only: false})
+      end
+
+      private :_time_for, :_parse_date, :_selections_for_coverage,
+        :_timestamps_in_range
+    end
   end
 end
